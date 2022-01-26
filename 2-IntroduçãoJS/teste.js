841ef146e9b51d02b895c225028ab6c339f9a45f
JonathasGit <jonathas-25@hotmail.com>
2022-01-26 00:23:58 -0200

    #2 codigo da aula

--- 2-IntroduçãoJS/teste.js
+++ 2-IntroduçãoJS/teste.js
@@ -20,3 +20,21 @@ function soma(a,b){
 }
 
 soma(10,5);
+
+console.log('Hello World');
+
+function returnEvenValues(array) {
+    let evenNums = []; //Lista vazia
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] % 2 ===0 ){ // 
+            evenNums.push(array[i]);
+        } else{
+            console.log( '${array[i]} não é par!') //  Retornar errado
+        }
+    }
+    console.log('Os números pares são: ' + evenNums); 
+}
+
+let array = [1, 2, 4, 5, 7, 8, 10, 11, 22, 13, 15, 17, 21, 30, 14, 31, 50];
+
+returnEvenValues(array);
