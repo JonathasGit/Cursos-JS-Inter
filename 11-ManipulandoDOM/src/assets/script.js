52a81cd792d2ea6c60ca0085dac5bb3ea6c7a800
JonathasGit <jonathas-25@hotmail.com>
2022-02-07 23:02:54 -0200

    #11 terminando o projeto

--- 11-ManipulandoDOM/src/assets/script.js
+++ 11-ManipulandoDOM/src/assets/script.js
@@ -1,11 +1,41 @@
 function changeMode() {
-    console.log('Clique pow');
+   changeClasses();
+   mudarTextos();
 }
 
+function changeClasses(){ // VAi adicionar as classes que não tem a classe dark mode
+    button.classList.toggle('dark-mode'); // Vai mudar as classes para essa dark mode
+    h1.classList.toggle('dark-mode');
+    body.classList.toggle('dark-mode');
+    footer.classList.toggle('dark-mode');
+    
+}
+
+function mudarTextos(){
+    const lightMode = "Light Mode"; 
+    const darkMode = "Dark Mode";
+
+
+    if(button.classList.contains(darkModeClass)) {
+        button.innerHTML = "Modo Claro"; 
+        h1.innerHTML = "Modo escuro Ligado";
+
+        return;
+    }
+    button.innerHTML = "Modo Escuro"; 
+        h1.innerHTML = "Modo claro Ligado";
+
+
+
+}
+
+const darkModeClass = 'dark-mode';
 const button = document.getElementById('modo-seleção');
 const h1 = document.getElementById('mudar');
 const body = document.getElementsByTagName('body') [0]; // Paramos em 8:25
-const body = document.getElementsByTagName('footer') [0];
+const footer = document.getElementsByTagName('footer') [0];
+
+
 
 
 
